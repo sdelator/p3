084570da7ce7653018a6eabac06f8261ab23e1bc
sdelator <sdelatorre@ucdavis.edu>
2015-02-07 12:00:08 -0800

    Update direct.cpp

--- direct.cpp
+++ direct.cpp
@@ -1,120 +1,123 @@
 // Author: Sean Davis
-#include <cstring>
-#include <iostream>
-#include <cstdlib>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "directory.h"
-using namespace std; 
 
-Directory::Directory(const char *nam, short umask, 
-                     int tim, Directory *paren):time(tim), subDirectories(NULL)\
-                     , subDirectoryCount(0), parent(paren)
-{
-  name = new char[strlen(nam) + 1];
-  set(permissions, 7, umask);
-}  // createDirectory())
 
-Directory* Directory::cd(Directory *directory, int argCount, const char *arguments[])
+Directory* cd(Directory *directory, int argCount, const char *arguments[])
 {
   if (argCount != 2)
   {
-    cout<<"usage: cd directoryName"<<endl;
+    printf("usage: cd directoryName\n");
     return directory;
   }  // if two many arguments
 
   if (strcmp(arguments[1], "..") == 0)
   {
-    if (parent)
-      return parent;
+    if (directory->parent)
+      return directory->parent;
     else  // this is root
       return directory;
   }  // if cd ..
 
-  for (int i = 0; i < subDirectoryCount; i++)
+  for (int i = 0; i < directory->subDirectoryCount; i++)
   {
-    if (strcmp(subDirectories[i]->name, arguments[1]) == 0)
-      return subDirectories[i];
+    if (strcmp(directory->subDirectories[i].name, arguments[1]) == 0)
+      return &directory->subDirectories[i];
   }  // for each subdirectory
  
-  cout<<"cd:" <<arguments[1]<< "No such file or directory"<<endl;
+  printf("cd: %s: No such file or directory\n", arguments[1]);
   return directory;
 }  // cd()
 
+void createDirectory(Directory *directory, const char *name, short umask, 
+                     int time, Directory *parent)
+{
+  directory->name = (char*) malloc(strlen(name) + 1);
+  directory->subDirectoryCount = 0;
+  directory->subDirectories = NULL;
+  strcpy(directory->name, name);
+  createPermissions(&directory->permissions, 7, umask);
+  directory->parent = parent;
+  directory->time = time;
+}  // createDirectory())
 
 
-void Directory::ls(const Directory *directory, int argCount, const char *arguments[])
+void ls(const Directory *directory, int argCount, const char *arguments[])
 {
   if (argCount > 2 || (argCount == 2 && strcmp(arguments[1], "-l") != 0))
-    cout<<"usage: ls [-l]"<<endl;
+    printf("usage: ls [-l]\n");
   else  // correct number of arguments
   {
     if (argCount == 1)  // simple ls
     {
       for (int i = 0; i < directory->subDirectoryCount; i++)
-        cout<<name<<subDirectories[i]->name<<endl;
+        printf("%s ", directory->subDirectories[i].name);
       
+      printf("\n");
     }  // if simple ls
     else  // must be ls -l
     {
       for (int i = 0; i < directory->subDirectoryCount; i++)
       {
-        print(subDirectories[i]->permissions);
-        cout<<time<<name<<subDirectories[i]->time<< 
-          subDirectories[i]->name<<endl;
+        printPermissions(&directory->subDirectories[i].permissions);
+        printf(" %d %s\n", directory->subDirectories[i].time, 
+          directory->subDirectories[i].name);
       }  // for each subdirectory
     }  // else is ls -l
   }  // else correct arguments
 }  // ls()
 
 
-void Directory::mkdir(Directory *directory,  int argCount, const char *arguments[], 
+void mkdir(Directory *directory,  int argCount, const char *arguments[], 
            short umask, int time)
 {
   if (argCount != 2)
   {
-    cout<<"usage: mkdir directory_name"<<endl;
+    printf("usage: mkdir directory_name\n");
     return;
   }  // if too many arguments
   
-  if (subDirectoryCount == MAX_DIRECTORIES)
+  if (directory->subDirectoryCount == MAX_DIRECTORIES)
   {
-    cout<<"mkdir: "<<name<< "already contains the maximum number of directories"
-            <<endl;
+    printf("mkdir: %s already contains the maximum number of directories\n",
+      directory->name);
     return;
   }  // if already at max subdirectories
   
-  for (int i = 0; i < subDirectoryCount; i++)
+  for (int i = 0; i < directory->subDirectoryCount; i++)
   {
-    if (strcmp(subDirectories[i]->name, arguments[1]) == 0)
+    if (strcmp(directory->subDirectories[i].name, arguments[1]) == 0)
     {
-      cout<<"mkdir: cannot create directory "<<arguments[1]<<": File exists" 
-        <<endl;
+      printf("mkdir: cannot create directory '%s': File exists\n", 
+        arguments[1]);
       return;
     }  // if subdirectory already exists.
   }  // for each subdirectory
   
-  Directory* *subDirectoriesTemp = 
-    new Directory*[sizeof(Directory) * (subDirectoryCount + 1)];
+  Directory *subDirectoriesTemp = (Directory*) 
+    malloc(sizeof(Directory) * (directory->subDirectoryCount + 1));
     
-  for (int i = 0; i < subDirectoryCount; i++)
-    subDirectoriesTemp[i] = subDirectories[i];
+  for (int i = 0; i < directory->subDirectoryCount; i++)
+    subDirectoriesTemp[i] = directory->subDirectories[i];
   
-  delete [] subDirectories;
-  subDirectories = subDirectoriesTemp;
-  Directory(arguments[1], umask, 
-                     time, directory);
-  
+  free(directory->subDirectories);
+  directory->subDirectories = subDirectoriesTemp;
+  createDirectory(&directory->subDirectories[directory->subDirectoryCount], 
+    arguments[1], umask, time, directory);
   directory->subDirectoryCount++; 
 }  // mkdir()
 
 
-void Directory::showPath(const Directory *directory)
+void showPath(const Directory *directory)
 {
   if (directory->parent == NULL)
   {
-    cout<<"/";
+    printf("/");
     return;
   }  // at root
   
-  showPath(parent);
-  cout<<name<<"/";
+  showPath(directory->parent);
+  printf("%s/", directory->name);
 }  // showPath())
